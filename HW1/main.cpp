f2f3b3deabd6e2fea859325d7ac587d7b6daa51d
Tyler Crouch <Plutonium130@gmail.com>
2020-09-23 17:17:06 -0700

    Added sectors

--- HW1/main.cpp
+++ HW1/main.cpp
@@ -5,14 +5,16 @@
 #include <array>
 #include <mpi.h>
 
-const int COUNT = 250000;
+const int COUNT = 100000000;
+const int XRANGE[] = {1,-1,-1,1};
+const int YRANGE[] = {1,1,-1,-1};
 
-std::array<std::pair<float,float>,COUNT>* generateCoords(int id){
+std::array<std::pair<double,double>,COUNT>* generateCoords(int id){
     srand(id);
-    std::array<std::pair<float,float>,COUNT> *coords = new std::array<std::pair<float,float>,COUNT>;
+    std::array<std::pair<double,double>,COUNT> *coords = new std::array<std::pair<double,double>,COUNT>;
     for(int i = 0; i < COUNT; i++){
-        float x = ((float)rand()) / ((float)RAND_MAX);
-        float y = ((float)rand()) / ((float)RAND_MAX);
+        double x = ((double)rand()) / ((double)RAND_MAX) * XRANGE[id%4];
+        double y = ((double)rand()) / ((double)RAND_MAX) * YRANGE[id%4];
         coords->at(i) = std::make_pair(x,y);
     }
     return coords;
@@ -24,10 +26,10 @@ int main(int argc,char **argv){
     MPI_Init(&argc,&argv);
     MPI_Comm_rank(MPI_COMM_WORLD,&id);
     MPI_Comm_size(MPI_COMM_WORLD,&size);
-    std::array<std::pair<float,float>,COUNT>* coordinates = generateCoords(id);
+    std::array<std::pair<double,double>,COUNT>* coordinates = generateCoords(id);
     int inside = 0;
     for(int i = 0; i < coordinates->size();i++){
-        float h = sqrt(pow(coordinates->at(i).first,2) + pow(coordinates->at(i).second,2));
+        double h = sqrt(pow(coordinates->at(i).first,2) + pow(coordinates->at(i).second,2));
         if(h <= 1){
             inside++;
         }
@@ -36,10 +38,9 @@ int main(int argc,char **argv){
     int totalInside;
     MPI_Reduce(&inside,&totalInside, 1, MPI_INT, MPI_SUM, 0,MPI_COMM_WORLD);    
     if(id == 0){
-
-        float sum = (float)totalInside/(float)(COUNT*size);
-        float pi = sum * 4;
-        std::cout << "Rank: " << id << "\t" << "PI: " << pi << std::endl;
+        double ratio = (float)totalInside/(float)(COUNT*size);
+        double pi = ratio * 4;
+        std::cout << "COUNT PER NODE: " << COUNT << "\t" << "PI: " << pi << std::endl;
         delete coordinates;
     }
     MPI_Finalize(); 
