64f9fb039a37530065f824d3d41c7692f1b4640c
Tyler Crouch <Plutonium130@gmail.com>
2020-10-26 15:54:35 -0700

    HW2 Complete

--- HW2/monte.cpp
+++ HW2/monte.cpp
@@ -8,31 +8,32 @@
 #include <mutex>
 #include <chrono>
 
-#define MAX_THREADS 256
+#define MAX_THREADS 16
 #define MAX_POINTS 2000000000
-#define MIN_POINTS 10
+#define MIN_POINTS MAX_THREADS
 #define CHUNK_SIZE 100000000
 
-std::mutex iomutex;
+std::mutex sumMutex;
 
 class BadArgException : std::exception{
     public:
         virtual const char* what() const noexcept{
-            return "usage: monte [1...256] [10...2,000,000,000]";
+            return "usage: monte [1...16] [16...2,000,000,000]";
         }
 }badarg;
 
 void monte(std::vector<std::pair<double,double>> *nums, int startIndex, int endIndex, int &sum, int threadNum){
     int localSum = 0;
+
     for(int i = startIndex; i < endIndex; i++){
         double result = sqrt(pow(nums->at(i).first,2) + pow(nums->at(i).second,2));
         if(result <= 1){
             localSum++;
         }
     }
-    std::lock_guard<std::mutex> lock(iomutex);
+
+    std::lock_guard<std::mutex> lock(sumMutex);
     sum += localSum;
-    std::cout << "THREAD#" << threadNum << " LOCAL SUM: " << localSum << std::endl;
 }
 
 void generateNums(std::vector<std::pair<double,double>> *nums,int points){
@@ -53,30 +54,37 @@ void generateNums(std::vector<std::pair<double,double>> *nums, int startIndex, i
     }
 }
 void generateNumsP(std::vector<std::pair<double,double>> *nums, int threads, int points){
+   
     int slice = points/threads;
     int rem = points%threads;
     int localThreads = threads;
+
     if(rem != 0){
         localThreads++;
     }
+
     int startIndex = 0;
     std::thread *localPool = new std::thread[localThreads]; 
     void (*pFunc)(std::vector<std::pair<double,double>>*,int,int) = generateNums;
+
     for(int i = 0; i < localThreads ; i++){
         int endIndex = startIndex + slice;
         if(i == localThreads-1 && rem != 0){
-            endIndex -= (slice - rem);
+            endIndex = startIndex + rem;
         }
         localPool[i] = std::thread(pFunc, nums, startIndex, endIndex);
         startIndex += slice;
     }
 
     for(int i = 0; i < localThreads; i++){
-        localPool[i].join();
+        if(localPool[i].joinable()){
+            localPool[i].join();
+        }
     }
 
     delete[] localPool;
 }
+
 int main(int argc,char** argv){
     unsigned short threads;
     unsigned long points;
@@ -93,52 +101,64 @@ int main(int argc,char** argv){
         std::cerr << e.what() << std::endl;
         return 1;
     }
+
     int chunkCount = points/CHUNK_SIZE;
     int chunkRem = points%CHUNK_SIZE;
+
     if(chunkRem != 0 ){
         chunkCount++; 
     }
+    std::cout << "\nCHUNK SIZE: " << CHUNK_SIZE << std::endl
+            << "CHUNK COUNT: " << chunkCount << std::endl << std::endl;
     int sum = 0;
-    
+    auto start = std::chrono::system_clock::now();
+
     for(int i = 0; i < chunkCount; i++){
-        auto start = std::chrono::system_clock::now();
+
+        auto chunkStart = std::chrono::system_clock::now();
         int localPoints;
         if(i == chunkCount-1 && chunkRem){
             localPoints = chunkRem;
         }else{
             localPoints = CHUNK_SIZE;
         }
-        std::thread *pool = new std::thread[threads];
+        
         std::vector<std::pair<double,double>> *nums = new std::vector<std::pair<double,double>>(localPoints);
         generateNumsP(nums, threads, localPoints);
-
         int rem = localPoints%threads;
         int slice = localPoints/threads;
+
         if(rem != 0){
             threads++;
         }
+
+        std::thread *pool = new std::thread[threads];
         int startIndex = 0;
 
         for( int i = 0; i < threads; i++){
             int endIndex = startIndex+slice;
             if(i == threads-1 && rem != 0){
-                endIndex -= (slice-rem);
+                endIndex = startIndex + rem;
             }
             pool[i] = std::thread(monte, std::ref(nums), startIndex,endIndex, std::ref(sum),i);
             startIndex += slice;
-        }
+        }   
 
-        for(unsigned short i = 0; i < threads; i++){
-            pool[i].join();
+        for(int i = 0; i < threads; i++){
+            if(pool[i].joinable()){
+                pool[i].join();
+            }
         }
-        std::chrono::duration<double> dur = (std::chrono::system_clock::now() - start);
+        
+        std::chrono::duration<double> localDur = (std::chrono::system_clock::now() - chunkStart);
         std::cout << "Sum after chunk #" << i << ": " << sum << std::endl
-                    << "Time for Chunk Completion: " << dur.count() << std::endl;
+                    << "Time for Chunk Completion: " << localDur.count() << std::endl << std::endl;
 
         delete[] pool;
         delete nums;
     }
     
+    std::chrono::duration<double> totalDur = (std::chrono::system_clock::now() - start);
     double ratio = (double)sum/points;
     double pi = ratio * 4;
 
@@ -146,9 +166,10 @@ int main(int argc,char** argv){
     std::cout << "TOTAL POINTS: " << points << std::endl;
     std::cout << "RATIO: " << ratio << std::endl;
     std::cout << "PI APPROXIMATION: " << pi << std::endl;
+    std::cout << "TOTAL COMPLETION TIME: " << totalDur.count() << std::endl;
 
     return 0;
-}
+}   
 
 
 
